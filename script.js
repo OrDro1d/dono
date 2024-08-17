85d9fc2e90e81c9e2680f614d062ac4a7a0291ad
Osman Lyumanov <osmanlyumanov2005@gmail.com>
2024-08-17 21:21:59 +0300

    * Доработан интерфейс * Сайт теперь действительно позволяет создавать заметки * Заметки не создаются при пустом поле для текста

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-container = document.querySelector(".container")
-newNote = document.querySelector(".new-note")
-makeNote = document.querySelector(".make-note")
+notesNumber = Number(localStorage.getItem("notesNumber"));
 
-makeNote.addEventListener("click", (e) => {
-    container.style.alignItems = "start";
-    container.style.width = "700px";
-    newNote.hidden = false;
-})
+newNote = document.querySelector(".new-note");
+makeNote = document.querySelector(".make-note");
+newNoteText = document.querySelector(".new-note-text");
+
+makeNote.addEventListener("click", e => {
+	document.body.style.alignItems = "start";
+	newNote.hidden = false;
+	makeNote.innerHTML = "✔️ Создать заметку";
+
+	makeNote.addEventListener("click", e => {
+		if (newNoteText.value != "") {
+			console.log('"a"' + newNoteText.value + '"a"');
+			notesNumber++;
+			sessionStorage.setItem("note" + notesNumber, newNoteText.value);
+			createdNote = document.createElement("li");
+			createdNote.innerHTML = '<li class="note">' + newNoteText.value + "</li>";
+			newNoteText.value = "";
+			document.querySelector(".notes").appendChild(createdNote);
+		}
+	});
+});
+
+document.addEventListener("unload", e => {
+	localStorage.getItem("notesNumber", String(notesNumber));
+});
+document.add;
