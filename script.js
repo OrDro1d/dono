0c8d28e2bb8b578e59f93d345fe6b2d0f0fd17c7
Osman Lyumanov <osmanlyumanov2005@gmail.com>
2024-08-18 20:16:15 +0300

    * В js документе были добавлены комментарии для меня любимого * Теперь заметки сохраняются между сессиями использования сайта * Удалено нижнее меню за ненадобностью * Изменена иконка

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-notesNumber = Number(localStorage.getItem("notesNumber"));
-
-newNote = document.querySelector(".new-note");
-makeNote = document.querySelector(".make-note");
-newNoteText = document.querySelector(".new-note-text");
-
-makeNote.addEventListener("click", e => {
+"use strict";
+// Считываем количество заметок и сохраняем это значение
+let notesNumber = Number(localStorage.getItem("notesNumber"));
+const notesOldNumber = notesNumber;
+// Добавляем на страницу заметки созданные ранее
+for (let i = 0; i < notesNumber; i++) {
+	let createdNote = document.createElement("li");
+	createdNote.textContent = localStorage.getItem("note" + i);
+	createdNote.classList = "note";
+	document.querySelector(".notes").appendChild(createdNote);
+}
+// Ну тут все и так ясно, m'kay?
+const newNote = document.querySelector(".new-note");
+const makeNote = document.querySelector(".make-note");
+const newNoteText = document.querySelector(".new-note-text");
+// Создание заметки по нажатию кнопки
+makeNote.addEventListener("click", event => {
 	document.body.style.alignItems = "start";
 	newNote.hidden = false;
 	makeNote.innerHTML = "✔️ Создать заметку";
 
-	makeNote.addEventListener("click", e => {
+	makeNote.addEventListener("click", event => {
 		if (newNoteText.value != "") {
-			console.log('"a"' + newNoteText.value + '"a"');
-			notesNumber++;
-			sessionStorage.setItem("note" + notesNumber, newNoteText.value);
-			createdNote = document.createElement("li");
-			createdNote.innerHTML = '<li class="note">' + newNoteText.value + "</li>";
-			newNoteText.value = "";
+			let createdNote = document.createElement("li");
+			createdNote.textContent = newNoteText.value;
+			createdNote.classList = "note";
 			document.querySelector(".notes").appendChild(createdNote);
+
+			sessionStorage.setItem("note" + notesNumber, newNoteText.value);
+			newNoteText.value = "";
+
+			notesNumber++;
 		}
 	});
 });
-
-document.addEventListener("unload", e => {
-	localStorage.getItem("notesNumber", String(notesNumber));
+// Загрузка заметок в localStorage перед закрытием страницы
+window.addEventListener("unload", event => {
+	localStorage.setItem("notesNumber", String(notesNumber));
+	for (let i = notesOldNumber; i < notesNumber; i++) {
+		localStorage.setItem("note" + i, sessionStorage.getItem("note" + i));
+	}
 });
-document.add;
